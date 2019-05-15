2f2208ab864ef3a7db80e93cd06f1155a5606bde
erikikoo <erikikoo@hotmail.com>
2019-05-14 22:08:59 -0300

    'upd'

--- config/application.rb
+++ config/application.rb
@@ -31,37 +31,14 @@ module SoftcolorApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+   
 
-    
-    # config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
-    #   allow do
-    #       origins '*'
-    #       resource '*',
-    #           headers: :any,
-    #           methods: [:get, :post, :delete, :put, :patch, :options, :head],
-    #           max_age: 0,
-    #           expose: :location
-    #       end
-    #   end
-
-    # config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
-    #   allow do
-    #     origins '*'
-    
-    #     resource '*',
-    #       headers: :any,
-    #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    #   end
-    # end
-
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options, :head]
       end
-    end
-
-  end
+    end  
     
     config.middleware.use Rack::Attack
 
