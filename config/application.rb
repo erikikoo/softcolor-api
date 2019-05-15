dcfeb7e7b170944df3fab6f3941878815008706a
erikikoo <erikikoo@hotmail.com>
2019-05-14 21:39:24 -0300

    'upd'

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,7 @@ module SoftcolorApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :delete]
+        resource '*', headers: :any, methods: [:get, :post, :options, :delete, :put]
       end
     end
     
