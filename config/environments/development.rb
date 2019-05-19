89fda6099f9139bc3fd9100a885061d3f23b78bf
erikikoo <erikikoo@hotmail.com>
2019-05-19 20:24:15 -0300

    'addSendOrcamento'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,9 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
   
-  config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
+
 
 #   config.action_mailer.smtp_settings = {
 #     address: "smtp.gmail.com",
