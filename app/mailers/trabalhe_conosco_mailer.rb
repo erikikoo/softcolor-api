ec9ea0e8724157935590a28fd9eb7e7c15fad571
erikikoo <erikikoo@hotmail.com>
2019-06-01 11:21:58 -0300

    'update'

--- app/mailers/trabalhe_conosco_mailer.rb
+++ app/mailers/trabalhe_conosco_mailer.rb
@@ -2,12 +2,12 @@ class TrabalheConoscoMailer < ApplicationMailer
     def send_trabalhe_conosco
         @orcamento = params[:orcamento]    
         
-        if (@orcamento['arte'])
+        if (params[:orcamento][:arte])
             ext = @orcamento['arte'].original_filename.split(".").last         
     
             attachments["curriculo.#{ext}"] = File.read(@orcamento['curriculo'].path())
         end
-        
+
         mail(to: "erikikoo@hotmail.com", subject: 'Curriculo - Site')
       end
 end
