655923889965946641a5d1959fde203644182731
erikikoo <erikikoo@hotmail.com>
2019-06-04 18:59:05 -0300

    'update'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,7 @@ Rails.application.configure do
 
     config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 587,
-    domain: 'gmail.com',  
+    port: 587,     
     user_name: '240382013@eniac.edu.br',
     password: 'HayHelena',
     authentication: "plain",
