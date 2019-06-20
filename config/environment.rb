0b8a393d377f049f4ee55df639f02cf870de842d
erikikoo <erikikoo@hotmail.com>
2019-06-20 15:24:01 -0300

    'update

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 
  ActionMailer::Base.smtp_settings = {
-    :user_name => ENV["SENDGRID_EMAIL"],
+    :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_SENHA"],    
     :address => ENV["SENDGRID_ADDRESS"],
     :port => 587,
