0c91b589920d71440fb054a610c76e53b0895d4d
erikikoo <erikikoo@hotmail.com>
2019-06-20 14:59:42 -0300

    'update

--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,9 @@ Rails.application.initialize!
 
 
  ActionMailer::Base.smtp_settings = {
-    :user_name => ENV["SENDGRID_USERNAME"],
-    :password => ENV["SENDGRID_SENHA"],
-    :address => 'smtp.sendgrid.net',
-    # :address => 'smtp.softcolor.com.br',
+    :user_name => ENV["UOL_EMAIL"],
+    :password => ENV["UOL_SENHA"],    
+    :address => ENV["UOL_ADDRESS"],
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
